7b48b0115cc470397ec2fcec207ba6f02e43e541
MayconWSoler <31396231+MayconWSoler@users.noreply.github.com>
2018-07-04 21:56:29 -0300

    Final
    Finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 import search from "./character-search";
+import search2 from "./event-search";
 
 const form = document.querySelectorAll("[data-form]");
 
@@ -10,4 +11,14 @@ form[0].addEventListener("submit", event => {
   const formData = new FormData(formComponent);
 
   search(formData.get("heroName"));
+});
+
+form[1].addEventListener("submit", event => {
+  event.preventDefault();
+
+  const formComponent = event.target;
+
+  const formData = new FormData(formComponent);
+
+  search2(formData.get("eventName"));
 });
--- assets/javascript/event-search.js
+++ assets/javascript/event-search.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 import md5 from "md5";
+import char from "./character-search";
 
 export default name => {
   const ts = new Date().getTime();
   const publicKey = "2fff075ddfa138a316ccb4a90c088b34";
   const privateKey = "18afb16cbc7ea935b10b7d9b3293e569a3fe421f";
-  const heroName = name;
+  const eventName = name;
 
   const hash = md5(`${ts}${privateKey}${publicKey}`);
   const instance = axios.create({
-    baseURL: `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${heroName}&apikey=${publicKey}&hash=${hash}`,
+    baseURL: `https://gateway.marvel.com:443/v1/public/events?nameStartsWith=${eventName}&apikey=${publicKey}&hash=${hash}`,
     timeout: 10000
   });
 
